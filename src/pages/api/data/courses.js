662b41391ce9b578312835c71a8e760c42fd03d8
Jasumasa <thejasumasa@gmail.com>
2022-04-06 14:24:08 +0900

    gut all old api files

--- src/pages/api/data/courses.js
+++
@@ -1,8 +0,0 @@
-import { courses } from "../../../data/coursedata";
-
-export default async function (req, res) {
-  if (req.method === "GET") {
-    console.log("courses API Accessed");
-    res.status(200).json(courses);
-  }
-}
